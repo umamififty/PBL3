7a5f9f4a01b1ff3ac90ba6173b8b181ecede99f8
zhenyu <90232193+umamififty@users.noreply.github.com>
2023-06-13 22:01:05 +0900

    wazza

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "black";
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "black";
   }
