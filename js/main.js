80187cdcd6f7d8056448aaab131250d9fb0e443c
zhenyu <90232193+umamififty@users.noreply.github.com>
2023-07-04 22:27:49 +0900

    you fool!

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "#313131";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "#313131";
-  }
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "#313131";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "#313131";
+}
+
+function openModal() {
+  modal.style.height = '75%';
+  // document.body.style.opacity = '0.5';
+}
+
+function closeModal() {
+  var modal = document.getElementById('modal');
+  modal.style.height = '0%';
+  document.body.style.backgroundColor = "#313131";
+  document.body.style.color = "#ffffff";
+}
