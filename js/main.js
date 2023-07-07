694ef8cf98c59b7d5ee174b39018da4f961ca6eb
zhenyu <90232193+umamififty@users.noreply.github.com>
2023-07-07 16:30:23 +0900

    febfiefiueb

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "15%";
-  document.getElementById("main").style.marginLeft = "15%";
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "#313131";
 }
 
